6e16e5d1258293a059b268ccafd46c829a7326b9
James.Zhang <james.zhang.hkust@gmail.com>
2017-07-27 07:47:58 +0800

    Finish market data h

---
+++ Yi_maket_receiver.h
@@ -0,0 +1,41 @@
+//used to receive market
+#ifndef YI_MD_API
+#define YI_MD_API
+#include<iostream>
+#include<string>
+#include"TradeApi/CThostFtdcMdApi.h"
+using namespace std;
+
+namespace Yi{
+class MyMarketAPi: public CThostFtdcMdSpi{
+public:
+	//use mdapi to construct an instance
+	MyMarketAPi(CThostFtdcMdApi *mdapi);
+	//construct connection
+	void OnFrontConnected();
+	//Response the login request
+	void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+	//Response the logout request
+	void OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	//require market data
+	void OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	//cancel market data
+	void OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	//disconnected
+	void OnFrontDisconnected(int nReason){};	
+
+	//Response the logout request
+	void OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+	
+
+private:
+	CThostFtdcMdApi *mymdapi;
+	CThostFtdcReqUserLoginField *mylogin;
+	int myloginID;
+}
+
+
+}
---
+++ Yi_parameters.h
@@ -0,0 +1,7 @@
+//system related parameters
+#define VERSION 1.0 //version of the system
+#define BROKER_ID "1008"
+#define TRADE_FRONT_IP "180.168.146.187:10000" 
+#define MARKET_FRONT_IP "180.168.146.187:10010"
+#define USER_ID "aaa"
+#define USER_PWD "bbb"
